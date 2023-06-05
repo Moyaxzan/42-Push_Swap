d1d0bf2879139711d943207af55bc1e663759954
Tao <tao.spa2003@gmail.com>
2023-06-05 13:43:26 +0200

    correct rrevert ?

--- rrevert.c
+++ rrevert.c
@@ -6,7 +6,7 @@
 /*   By: tsaint-p <tsaint-p@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/04 20:26:37 by tsaint-p          #+#    #+#             */
-/*   Updated: 2023/06/04 21:59:23 by tsaint-p         ###   ########.fr       */
+/*   Updated: 2023/06/05 13:43:08 by tsaint-p         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -22,10 +22,9 @@ void	rrevert(t_list **stack)
 		while ((*stack)->next->next)
 			*stack = (*stack)->next;
 		(*stack)->next->next = tmp;
-		(*stack)->next = tmp;
-		tmp = *stack;
-		*stack = (*stack)->next;
-		tmp->next = 0x0;
+		tmp = (*stack)->next;
+		(*stack)->next = 0x0;
+		*stack = tmp;
 	}
 }
 
