d52af14e26b1ed24e6ec026830ac43a9af8a6120
Tao <tao.spa2003@gmail.com>
2023-06-05 12:55:26 +0200

    better main to test basics

--- main.c
+++ main.c
@@ -3,6 +3,26 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+void	print_stacks(t_list *stack_a, t_list *stack_b)
+{
+	while (stack_a || stack_b)
+	{
+		if (stack_a)
+		{
+			printf("%s", (char *)stack_a->content);
+			stack_a = stack_a->next;
+		}
+		else
+			printf(" ");
+		if (stack_b)
+		{
+			printf("\t%s", (char *)stack_b->content);
+			stack_b = stack_b->next;
+		}
+		printf("\n");
+	}
+}
+
 int main(int argc, char **argv)
 {
 	t_list	*node;
@@ -21,11 +41,21 @@ int main(int argc, char **argv)
 		ft_lstadd_back(&stack_a, node);
 		i++;
 	}
-	node = stack_a;
-	while (node)
-	{
-		printf("%s ", (char *)node->content);
-		node = node->next;
-	}
+	print_stacks(stack_a, stack_b);
+	push(&stack_a, &stack_b);
+	printf("\npb\n");
+	push(&stack_a, &stack_b);
+	printf("pb\n");
+	swap(&stack_a);
+	printf("sa\n");
+	revert(&stack_a);
+	printf("ra\n");
+	push(&stack_b, &stack_a);
+	printf("pa\n");
+	revert(&stack_a);
+	printf("ra\n");
+	push(&stack_b, &stack_a);
+	printf("pa\n");
+	print_stacks(stack_a, stack_b);
 	return (0);
 }
